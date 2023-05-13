4c1b2bfd2e229b504c949ba991cc45ea16278ff1
henry eschenmoser <henry.eschenmoser@gmail.com>
2023-05-13 11:41:00 +0200

    removed old not working code

--- three_tutorial/main.js
+++ three_tutorial/main.js
@@ -1,26 +1,4 @@
 import * as THREE from 'three';
-
-// Video tutorial code that doesn't work at the moment
-// const renderer = new THREE.WebGLRenderer();
-
-// renderer.setSize(window.innerWidth, window.innerHeight);
-
-// document.body.appendChild(renderer.domElement);
-
-// const scene = new THREE.PerspectiveCamera(
-//   75,
-//   window.innerWidth / window.innerHeight,
-//   0.1,
-//   1000
-// );
-
-// const axesHelper = new THREE.AxesHelper(5);
-// scene.add(axesHelper);
-
-// camera.position.z = 5;
-
-// renderer.render(scene, camera);
-
 // Threejs documentation tutorial
 
 // Setting up a new scene
